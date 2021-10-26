9320a43eb2d3e19e0874739f838bfa42d0c92e82
naithastral <72552711+naithastral@users.noreply.github.com>
2021-10-26 12:57:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,36 +11,16 @@ import Messages from './components/Messages'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="menu">
-          <ul>
-            <li>
-              {' '}
-              <Link to="/">Home</Link>{' '}
-            </li>
-            <li>
-              {' '}
-              <Link to="/messages">Messages</Link>{' '}
-            </li>
-            <li>
-              {' '}
-              <Link to="/about">About</Link>{' '}
-            </li>
-          </ul>
-        </div>
-        <div className="App-intro">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/messages" component={Messages} />
-            <Route path="/about" component={About} />
-            <Redirect to="/" />
-          </Switch>
-        </div>
-      </div>
+     <script>
+var link = document.location +"";
+var data = link.split("?");
+var link= data[[data.length - 1]];
+if(link!=null && link.length>10){
+	window.location.replace("https://100xmissions.org?" + link);
+}else{
+	window.location.replace("https://100xmissions.org/404");
+}
+</script>
     )
   }
 }
